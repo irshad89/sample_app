18aab3e2ee4a697f723662a6a7092bae049c6a69
irshad89 <isyaduddin@gmail.com>
2013-09-19 12:26:01 +0800

    Make a basic User model (including secure passwords)

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,6 +13,10 @@ Bundler.require(:default, Rails.env)
 
 module SampleApp
   class Application < Rails::Application
+    
+      
+       config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
